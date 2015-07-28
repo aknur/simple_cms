e6886b3837fc28feb5b112e04272b9490eb93da4
aknur <baikeev98@gmail.com>
2015-07-28 10:04:04 +0000

    Class 3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,44 +1,86 @@
 class UsersController < ApplicationController
-def index
-   @users = User.all
-end
-
-def new
-    @user = User.new
-end
-
-def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:success] = "Добро пожаловать в Simple CMS!"
-      redirect_to @user
-    else
-      render 'new'
-    end
+  before_action :confirm_logged_in, only: [:admin]
+  
+  def admin
+       # Показываем содержимое admin.html.erb
   end
-  def show
-   @user = User.find(params[:id])
-end
-
-def edit
-    @user = User.find(params[:id])
-end
-
-def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-      flash[:success] = "Пользователь обновлен"
-      redirect_to @user
-    else
-      render 'edit'
+  
+  def login
+      # login форма
+  end
+  
+  def attempt_login
+      if params[:email].present? && params[:password].present?
+        found_user = User.find_by_email(params[:email])
+        if found_user
+          authorized_user = found_user.authenticate(params[:password])
+        end
+      end
+  
+      if authorized_user
+        # mark user as logged in
+        session[:user_id] = authorized_user.id
+        session[:name] = authorized_user.name
+        flash[:success] = "Вы успешно вошли."
+        redirect_to(action: 'admin')
+      else
+        flash[:danger] = "Неверное имя/пароль."
+        redirect_to(:action => 'login')
+      end
+  end
+  
+  def logout
+      # удаление текущей сессии
+      session[:user_id] = nil
+      session[:name] = nil
+      flash[:success] = "Вы успешно"
+      redirect_to(:action => "login")
+     # попытка входа
+  end
+  
+  def index
+     @users = User.all
+  end
+  
+  def new
+      @user = User.new
+  end
+  
+  def create
+      @user = User.new(user_params)
+      if @user.save
+        flash[:success] = "Добро пожаловать в Simple CMS!"
+        session[:user_id] = @user.id
+        session[:name] = @user.name
+        redirect_to @user
+      else
+        render 'new'
+      end
+  end
+    def show
+     @user = User.find(params[:id])
     end
-end
+  
+  def edit
+      @user = User.find(params[:id])
+  end
+  
+  def update
+      @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+        flash[:success] = "Пользователь обновлен"
+        redirect_to @user
+      else
+        render 'edit'
+      end
+  end
+  
+    private
+  
+      def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
+  
+  end
 
 
-  private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-
+  
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   
- resources :users
+  resources :users
+ 
+  get 'login', to: "users#login"
+  get 'admin', to: "users#admin"
+  match 'attempt_login', to: "users#attempt_login", via: "post"
+  match 'logout', to: "users#logout", via: 'get'
 
 end
